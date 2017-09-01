bc8d779d1d0648fbef5c08c959e00fd677fe7b54
Kumud Ravisankaran <kumudriyer@gmail.com>
2017-09-01 00:16:42 -0500

    minute changes to add features into the database

--- www/js/umpireScreen.js
+++ www/js/umpireScreen.js
@@ -13,17 +13,20 @@ function openCity(cityName,elmnt,color) {
 
 }
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-var mainText = document.getElementById(mainText);
-var submitBtn = document.getElementById(submitBtn);
-var database = firebase.database();
+
 
 
 function submitClick()
+
 {
+    
+     
+    var submitBtn = document.getElementById(submitBtn);
+    var database = firebase.database();
+   
     var firebaseRef = database.ref();
-    var messageText = mainText.value;
+    var messageText = document.getElementById("mainText").value;
     console.log(messageText);
-    firebaseRef.child("Text").set(messageText); 
+    firebaseRef.push().set(messageText); 
    
 }
