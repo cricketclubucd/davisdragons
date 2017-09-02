8026c0aaabc5518b99d9131938da604be5e90241
Suraj Jena <jena.suraj.k@gmail.com>
2017-09-02 00:34:29 -0700

    Revert "Rewinding old commits"
    This reverts commit 21b165e34e4903e46fc2bf29940765db9b0e6728.

--- www/js/umpireScreen.js
+++ www/js/umpireScreen.js
@@ -1,5 +1,3 @@
-function openCity(cityName,elmnt,color) {
-    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -10,23 +8,15 @@ function openCity(cityName,elmnt,color) {
     }
     document.getElementById(cityName).style.display = "block";
     elmnt.style.backgroundColor = color;
+ }
 
-}
+ function score () {
+   var a = 20;
+   return a;
+ }
+
+ document.getElementById("scoring").innerHTML = window.score();
+
+
 // Get the element with id="defaultOpen" and click on it
-
-
-
-function submitClick()
-
-{
-    
-     
-    var submitBtn = document.getElementById(submitBtn);
-    var database = firebase.database();
-   
-    var firebaseRef = database.ref();
-    var messageText = document.getElementById("mainText").value;
-    console.log(messageText);
-    firebaseRef.push().set(messageText); 
-   
-}
+document.getElementById("defaultOpen").click();
