5481ca86047190ff79d90328610215057064055b
Kumud Ravisankaran <kumudriyer@gmail.com>
2017-09-10 02:50:05 -0400

    Updating the data pushing functionality of umpire screen

--- src/pages/umpire/umpire.ts
+++ src/pages/umpire/umpire.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {AngularFireDatabase} from 'angularfire2/database';
+
 @Component({
 
   templateUrl: 'umpire.html'
 })
 export class UmpirePage {
  score = 0;
-  constructor(public navCtrl: NavController) {
- this.score = 0;
+ ball = 0;
 
-}
+  constructor(public navCtrl: NavController,  private fdb: AngularFireDatabase)
+   {
+     this.score = 0;
+
+ }
+ updateballid()
+ {
+   this.ball = this.ball+1;
+
+ }
+ pushdata()
+ {
+   var data = {
+     runs : this.score
+   }
+
+
+   this.fdb.list('/balls/').push(data);
+ }
 incrementone()
 {
   this.score = this.score +1;
