34991aefe21b31de1c0f00611da5b54bc826c2e4
Suraj Jena <jena.suraj.k@gmail.com>
2017-09-14 20:09:33 -0700

    New team template

--- src/models/team.ts
+++ src/models/team.ts
@@ -1,5 +1,18 @@
 
 export interface team
 {
-  name: string;
+  captain: number;
+  p1: number;
+  p2: number;
+  p3: number;
+  p4: number;
+  p5: number;
+  p6: number;
+  p7: number;
+  p8: number;
+  p9: number;
+  p10: number;
+  p11: number;
+  vcaptain: number;
+  wk: number;
 }
