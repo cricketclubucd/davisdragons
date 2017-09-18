d35c4437fbd664a74267f8b79b381a221becdacc
Suraj Jena <jena.suraj.k@gmail.com>
2017-09-18 16:36:04 -0700

    Displays latest match scores async’ed

--- src/pages/spectator/spectator.ts
+++ src/pages/spectator/spectator.ts
@@ -4,6 +4,8 @@ import { Platform, ActionSheetController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
 import { User } from '../../models/user';
+import { key } from '../../models/match';
+import {score} from '../../models/Score';
 import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 import 'rxjs/add/operator/do';
 
@@ -14,13 +16,16 @@ import 'rxjs/add/operator/do';
 })
 
 
-export class SpectatorPage {
-
-
+export class SpectatorPage
+{	
+  name: any;
+  key = {} as key;
+  score = {} as score;
   scoreRef$: FirebaseListObservable<any[]>
   matchStats$: FirebaseListObservable<any[]>
   playersTeamA$: FirebaseListObservable<any[]>
   playersTeamB$: FirebaseListObservable<any[]>
+  data : FirebaseListObservable<any>;
 
   roster: string = "Scoring";
   isAndroid: boolean = false;
@@ -33,8 +38,20 @@ export class SpectatorPage {
   {
     this.isAndroid = platform.is('android');
     this.roster = "vs.";
-    this.matchStats$ = this.database.list('Matches/Match1/MatchStats');
-    this.scoreRef$ = this.database.list('Matches/Match1/Balls');
+    this.score.ballPtr = 0;
+    this.score.totalRuns =0;
+    this.score.totalOvers = "";
+    this.score.totalWickets =0;
+    this.key.MatchKey = "0";
+    this.name = this.database.object('/ClubParams/LiveMatchState/');
+    this.name.take(1).subscribe(data =>
+    {
+        console.log("Match Ptr: " + data.matchPtr);
+        this.key.MatchKey= data.matchPtr;
+    });// Finds out the current matchPtr
+
+    this.scoreRef$ = this.database.list(`/Matches/` + this.key.MatchKey + `/MatchStats/Score/`);
+    this.matchStats$ = this.database.list(`/Matches/` + this.key.MatchKey + `/MatchStats/`);
     this.playersTeamA$ = this.database.list('Matches/Match1/MatchStats/PlayerRoster/Home');
     this.playersTeamB$ = this.database.list('Matches/Match1/MatchStats/PlayerRoster/Away');
     this.playersTeamA$.subscribe(x => console.log(x))
