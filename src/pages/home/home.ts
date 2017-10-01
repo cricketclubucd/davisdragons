f28624bc3d36d7d2fc6d45455f513a6bf0838e1b
Suraj Jena <jena.suraj.k@gmail.com>
2017-09-30 19:10:34 -0700

    Redirect to appropriate Match (View/Ctrlr) Page based on AccessLevel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { SpectatorPage } from '../spectator/spectator';
+import{ MemberPage } from '../member/member';
+import { UmpirePage } from '../umpire/umpire';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  isLive: boolean = false;
+  matchState: string = "No Live Match";
+  intMatchState: number = 0;
+  accessLevel: number = 5;
   name: any;
   static database: any;
   constructor(public navCtrl: NavController, public fdb: AngularFireDatabase) {
   	HomePage.database = fdb;
+  	this.accessLevel = 5;
+  	this.name = this.fdb.object('/ClubParams/LiveMatchState/');
+  	this.name.take(1).subscribe(data =>
+    {
+        console.log("Match State: " + data.matchState);
+        this.intMatchState = data.matchState;
+        if (this.intMatchState === 1)
+        {
+	        this.isLive = true;
+	        this.matchState = "Match Live!";
+        }
+        this.resolveAccessLevel();
+    });
   }
   static getCurrentMatch()
   {
@@ -24,5 +44,33 @@ export class HomePage {
     });// Finds out the current matchPtr
     return key;
   }
-
+  goToMatch()
+  {
+	 if (this.intMatchState === 1)
+	 {
+	 	if (this.accessLevel === 0 || this.accessLevel === 1)
+	 	{
+	 		this.navCtrl.setRoot(UmpirePage);
+		}
+		else
+		{
+			this.navCtrl.setRoot(SpectatorPage);
+		}
+	}
+  }
+  resolveAccessLevel()
+  {
+	  var playerKey = "75";
+	  this.fdb.list(`/ClubParams/AccessLevel/`, { preserveSnapshot: true})
+	  	.subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          console.log(snapshot.key, snapshot.val());
+          if (snapshot.key === playerKey)
+          {
+          	this.accessLevel = snapshot.val();
+		  	console.log("Access level is: " + this.accessLevel);
+		  }
+        });
+    });
+  }
 }
